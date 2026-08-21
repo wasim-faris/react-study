8ddf05d7426202277e5ff88e47045c4c3c9dd7e6
wasim-faris <waseemfaris179@gmail.com>
2026-08-21 17:42:57 +0530

    feat: fetch and display products from API

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,34 @@
-import Timer from "./components/timer/Timer";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(()=>{
+   fetch("https://dummyjson.com/products")
+   .then((response)=> response.json())
+   .then((data)=>{
+    setProducts(data.products)
+   })
+  })
+
   return (
-    <>
-    <Timer/>
-    </>
+    <div>
+      <h1>
+        Products
+      </h1>
+      {products.map((product)=>(
+        <div key={product.id}>
+          <p>
+            {product.title} 
+          </p>
+          <p>
+            {product.price}
+          </p>
+        </div>
+      ))}
+    </div>
   )
 }
+
 
 export default App;
